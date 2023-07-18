dc00d0f548c8c2f52fb376e111177f887a26110b
Artem <Kuzmichev8-1991@list.ru>
2023-07-18 22:46:50 +0300

    FiveExample

---
+++ Five_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter name of user ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Yra this is Mary");   
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
